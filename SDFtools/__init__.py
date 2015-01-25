fcfce66dc837aefa7676ad4757610c361e4b88fd
Kerry Hatcher <kwhatcher@gmail.com>
2015-01-25 10:32:46 -0500

    Heroku

--- SDFtools/__init__.py
+++ SDFtools/__init__.py
@@ -8,14 +8,16 @@ from os.path import expanduser
 from flask.ext.stormpath import StormpathManager
 from flask.ext import menu
 from routes import gui
+from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(gui)
 Bootstrap(app)
 
-app.config['SECRET_KEY'] = "734yt98473yt734ytc98y3tn98y897r67no"
-app.config['STORMPATH_API_KEY_FILE'] = expanduser('~/.stormpath')
-app.config['STORMPATH_APPLICATION'] = 'GSDF'
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['STORMPATH_API_KEY_ID'] = environ.get('STORMPATH_API_KEY_ID')
+app.config['STORMPATH_API_KEY_SECRET'] = environ.get('STORMPATH_API_KEY_SECRET')
+app.config['STORMPATH_APPLICATION'] = environ.get('STORMPATH_URL')
 
 
 stormpath_manager = StormpathManager(app)
