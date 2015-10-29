92a9f606fe7a521d18cf151431fa079575597e24
Kerry Hatcher <kwhatcher@gmail.com>
2015-10-29 13:10:12 -0400

    twilio hello world

--- SDFtools/phone/__init__.py
+++ SDFtools/phone/__init__.py
@@ -1,11 +1,17 @@
 __author__ = 'kwhatcher'
 
 from flask import Blueprint
-
+import twilio.twiml
 
 phone = Blueprint('phone', __name__)
 
 
-@phone.route('/')
-def hello_world():
-    return "hello world"
+
+
+@phone.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
