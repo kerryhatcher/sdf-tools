7ce808c370d7fe97254ca2ce4c35864a512b9985
Kerry Hatcher <kwhatcher@gmail.com>
2015-10-29 13:20:10 -0400

    fix hello_*

--- SDFtools/phone/__init__.py
+++ SDFtools/phone/__init__.py
@@ -9,7 +9,7 @@ phone = Blueprint('phone', __name__)
 
 
 @phone.route("/sms", methods=['GET', 'POST'])
-def hello_monkey():
+def hello_sms():
     """Respond to incoming calls with a simple text message."""
     from_number = request.values.get('From', None)
 
@@ -21,7 +21,7 @@ def hello_monkey():
 
 
 @phone.route("/voice", methods=['GET', 'POST'])
-def hello_monkey():
+def hello_voice():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
     resp.say("Hello Monkey")
